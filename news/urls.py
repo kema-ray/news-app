d2e96fd63cee978dbc46b8752e698605ee1d998d
rachel <rachelkemuma99@gmail.com>
2022-05-23 05:48:02 +0300

    Create the news structure and template inherirance

---
+++ news/urls.py
@@ -0,0 +1,8 @@
+from django.urls import re_path
+from . import views
+
+urlpatterns=[
+    re_path('^$',views.welcome,name='welcome'),
+    re_path('^today/$',views.news_of_day,name='newsToday'),
+    re_path(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews')
+]
